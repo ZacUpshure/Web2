eb0620e1f2a331abd368e56962f5bcec22fe27cd
Zac <zac.upshure@outlook.com>
2025-04-24 02:20:03 +0200

    fixed config with commonjs

--- dist/endpoints/user/UserService.js
+++ dist/endpoints/user/UserService.js
@@ -1,2 +1,77 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.createUserService = createUserService;
+exports.findUserByIdService = findUserByIdService;
+exports.findAllUsersService = findAllUsersService;
+exports.updateUserService = updateUserService;
+exports.deleteUserService = deleteUserService;
+const UserModel_1 = require("./UserModel");
+// create
+async function createUserService(data) {
+    const { userID, password } = data;
+    // Check if user already exists
+    const existingUser = await UserModel_1.UserModel.findOne({ userID });
+    if (existingUser) {
+        throw new Error('UserAlreadyExists');
+    }
+    // Create new user (password gets hashed via Mongoose hook)
+    const newUser = new UserModel_1.UserModel(data);
+    const savedUser = await newUser.save();
+    return {
+        userID: savedUser.userID,
+        firstName: savedUser.firstName,
+        lastName: savedUser.lastName,
+        isAdministrator: savedUser.isAdministrator
+    };
+}
+// search by ID
+async function findUserByIdService(userID) {
+    if (isNaN(userID))
+        throw new Error('InvalidUserID');
+    const user = await UserModel_1.UserModel.findOne({ userID });
+    if (!user)
+        throw new Error('UserNotFound');
+    return {
+        userID: user.userID,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        isAdministrator: user.isAdministrator
+    };
+}
+// read all
+async function findAllUsersService() {
+    const users = await UserModel_1.UserModel.find();
+    return users.map(user => ({
+        userID: user.userID,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        isAdministrator: user.isAdministrator
+    }));
+}
+// update
+async function updateUserService(userID, updateData) {
+    const user = await UserModel_1.UserModel.findOneAndUpdate({ userID }, updateData, {
+        new: true,
+        runValidators: true
+    });
+    if (!user)
+        throw new Error('UserNotFound');
+    return {
+        userID: user.userID,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        isAdministrator: user.isAdministrator
+    };
+}
+// delete
+async function deleteUserService(userID) {
+    const user = await UserModel_1.UserModel.findOneAndDelete({ userID });
+    if (!user)
+        throw new Error('UserNotFound');
+    return {
+        userID: user.userID,
+        firstName: user.firstName,
+        lastName: user.lastName
+    };
+}
 //# sourceMappingURL=UserService.js.map
--- dist/httpServer.js
+++ dist/httpServer.js
@@ -1,45 +1,23 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || (function () {
-    var ownKeys = function(o) {
-        ownKeys = Object.getOwnPropertyNames || function (o) {
-            var ar = [];
-            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
-            return ar;
-        };
-        return ownKeys(o);
-    };
-    return function (mod) {
-        if (mod && mod.__esModule) return mod;
-        var result = {};
-        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
-        __setModuleDefault(result, mod);
-        return result;
-    };
-})();
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-const express = __importStar(require("express"));
-const app = express();
-app.get('/', (req, res) => {
-    res.send('Hello World, this is a change.');
-});
+const express_1 = __importDefault(require("express"));
+const mongoose_1 = __importDefault(require("mongoose"));
+const UserRoute_1 = require("./endpoints/user/UserRoute");
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+// ✅ Mounten der Routing-Datei unter dem Prefix
+app.use('/api/publicUsers', UserRoute_1.userRouter);
 // Server starten
 const server = app.listen(3000, () => {
     console.log(`Server running at http://localhost:3000`);
+});
+//Mongodb Verbindung aufbauen
+mongoose_1.default.connect("mongodb://localhost:27017").then(() => {
+    console.log("MongoDB connected");
+}).catch((e) => {
+    console.log("Connection failed");
 });
 //# sourceMappingURL=httpServer.js.map
